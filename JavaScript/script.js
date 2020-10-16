e6084b11f315c221162b125136c4abee8d297f02
mackilroy007 <mackilroy007@gmail.com>
2020-10-16 14:06:02 +0200

    restuarant, events and locations fully implemented

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,41 +16,61 @@ var arr = [];
 //base class Location that contains the common denominators
 //Location has a conflict with some lib file and needed to be renamed
 var LocationA = /** @class */ (function () {
-    function LocationA(a, b, c, d) {
+    function LocationA(a, b, c, d, name) {
         this.city = a;
         this.zipCode = b;
         this.address = c;
         this.img = d;
+        this.name = name;
         //push content into the empty array
         arr.push(this);
     }
     //default displayment of content
     LocationA.prototype.render = function () {
-        return "\n                " + this.city + " " + this.zipCode + " " + this.address + " " + this.img + "\n        ";
+        return "\n        <div class=\"card\" style=\"width: 25rem;\">\n        <img src=\"" + this.img + "\" class=\"card-img-top\" alt=\"" + this.address + "\">\n        <div class=\"card-body\">\n            <ul>\n                <li class=\"card-text font-weight-bold\">Land mark: " + this.name + "</li>\n                <li class=\"card-text\">Location : " + this.address + ", " + this.zipCode + ", " + this.city + "</li>\n            </ul>\n        </div>\n      </div>\n        ";
     };
     return LocationA;
 }());
 var Restaurant = /** @class */ (function (_super) {
     __extends(Restaurant, _super);
-    function Restaurant(a, b, c, d, e, f, g) {
-        var _this = _super.call(this, a, b, c, d) || this;
-        _this.telephoneNumber = '';
-        _this.type = '';
-        _this.webAddress = '';
+    function Restaurant(a, b, c, d, name, e, f, g) {
+        var _this = _super.call(this, a, b, c, d, name) || this;
         _this.telephoneNumber = e;
         _this.type = f;
         _this.webAddress = g;
         return _this;
     }
     Restaurant.prototype.render = function () {
-        return "\n        <div class=\"card\" style=\"width: 20rem;\">\n        <img src=\"" + this.img + "\" class=\"card-img-top\" alt=\"" + this.type + " restaurant\">\n        <div class=\"card-body\">\n            <ul>\n                <li class=\"card-text font-weight-bold\">" + this.type + " Restaurant</li>\n                <li class=\"card-text\">Location : " + this.address + ", " + this.zipCode + ", " + this.city + "</li>\n                <li class=\"card-text\">Contact us under: " + this.telephoneNumber + " Restaurant</li>\n                <li class=\"card-text\">More info under: " + this.webAddress + " Restaurant</li>\n            </ul>\n        </div>\n      </div>\n        ";
+        return "\n        <div class=\"card \" style=\"width: 25rem;\">\n        <img src=\"" + this.img + "\" class=\"card-img-top\" alt=\"" + this.type + " restaurant\">\n        <div class=\"card-body\">\n            <ul>\n                <li class=\"card-text font-weight-bold\">" + this.name + " " + this.type + " Restaurant</li>\n                <li class=\"card-text\">Location : " + this.address + ", " + this.zipCode + ", " + this.city + "</li>\n                <li class=\"card-text\">Contact us under: " + this.telephoneNumber + "</li>\n                <li class=\"card-text\">More information <a href='" + this.webAddress + "'>here</a> </li>\n            </ul>\n        </div>\n      </div>\n        ";
     };
     return Restaurant;
 }(LocationA));
+var Events = /** @class */ (function (_super) {
+    __extends(Events, _super);
+    function Events(a, b, c, d, name, e, f, g, h, i) {
+        var _this = _super.call(this, a, b, c, d, name) || this;
+        _this.webAddress = e;
+        _this.eventDate = f;
+        _this.eventTime = g;
+        _this.ticketPrice = h;
+        _this.eventLocation = i;
+        return _this;
+    }
+    Events.prototype.render = function () {
+        return "\n        <div class=\"card\" style=\"width: 25rem;\">\n        <img src=\"" + this.img + "\" class=\"card-img-top\" alt=\"" + this.name + " restaurant\">\n        <div class=\"card-body\">\n            <ul>\n                <li class=\"card-text font-weight-bold\">" + this.name + "</li>\n                <li class=\"card-text\">Location :" + this.eventLocation + " " + this.address + ", " + this.zipCode + ", " + this.city + "</li>\n                <li class=\"card-text\">Event timings: " + this.eventTime + ", " + this.eventDate + "</li>\n                <li class=\"card-text\">Entry fee per person: " + this.ticketPrice + "\u20AC </li>\n                <li class=\"card-text\">More information <a href='" + this.webAddress + "'>here</a> </li>\n            </ul>\n        </div>\n      </div>\n        ";
+    };
+    return Events;
+}(LocationA));
+//landmark content
+new LocationA('Vienna', '1010', 'St. Charles Church', 'img/Karlskirche.jpg', 'St. Charles Church');
+new LocationA('Vienna', '1030', 'Maxingstraße 13b', 'img/Zoo.jpg', 'Vienna Zoo');
 //restaurant content
-new Restaurant('Vienna', '1050', 'Kettenbrückengasse 19', 'img/Lemon-leaf.png', '+43(1)5812308', 'Lemon Leaf Thai', 'www.lemonleaf.at');
-new Restaurant('Vienna', '1050', 'Schönbrunner Straße 21', 'img/Sixta.png', '+43 1 58 528 56 or +43 1 58 528 56', 'SIXTA Traditional Austrian', 'http://www.sixta-restaurant.at/');
-console.table(arr);
+new Restaurant('Vienna', '1050', 'Kettenbrückengasse 19', 'img/Lemon-leaf.png', 'Lemon Leaf', '+43(1)5812308', 'Thai', 'www.lemonleaf.at');
+new Restaurant('Vienna', '1050', 'Schönbrunner Straße 21', 'img/Sixta.png', 'SIXTA', '+43 1 58 528 56 or +43 1 58 528 56', 'Traditional Austrian', 'http://www.sixta-restaurant.at/');
+// event content
+new Events('Vienna', '1150', 'Roland Rainer Platz 1', 'img/Kris-k.jpg', 'Kris Kristofferson', 'http://kriskristofferson.com/', 'Fr., 15.11.2021', '20:00', '58,50', 'Wiener Stadthalle Halle F');
+new Events('Vienna', '1150', 'Roland Rainer Platz 1', 'img/Lenny-k.jpg', 'Lenny Kravitz', 'www.lennykravitz.com/', 'Sat., 09.112.2029', '19:30', '47,80', 'Wiener Stadthalle Halle D');
+// print out class content on to the html file using the arr
 for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
     var i = arr_1[_i];
     document.getElementsByClassName('result')[0].innerHTML += "" + i.render();
--- JavaScript/script.ts
+++ JavaScript/script.ts
@@ -8,11 +8,13 @@ class LocationA {
     zipCode;
     address;
     img;
-    constructor(a, b, c, d) {
+    name;
+    constructor(a, b, c, d, name) {
         this.city = a;
         this.zipCode = b;
         this.address = c;
         this.img = d;
+        this.name = name;
 
         //push content into the empty array
         arr.push(this);
@@ -21,31 +23,39 @@ class LocationA {
     //default displayment of content
     render() {
         return `
-                ${this.city} ${this.zipCode} ${this.address} ${this.img}
+        <div class="card" style="width: 25rem;">
+        <img src="${this.img}" class="card-img-top" alt="${this.address}">
+        <div class="card-body">
+            <ul>
+                <li class="card-text font-weight-bold">Land mark: ${this.name}</li>
+                <li class="card-text">Location : ${this.address}, ${this.zipCode}, ${this.city}</li>
+            </ul>
+        </div>
+      </div>
         `;
     }
 }
 
 class Restaurant extends LocationA {
-    telephoneNumber = '';
-    type = '';
-    webAddress ='';
-    constructor(a, b, c, d, e, f, g,) {
-        super(a, b, c, d);
+    telephoneNumber;
+    type;
+    webAddress;
+    constructor(a, b, c, d, name, e, f, g,) {
+        super(a, b, c, d, name);
         this.telephoneNumber = e;
         this.type = f;
         this.webAddress = g;
     }
-    render(){
-        return`
-        <div class="card" style="width: 20rem;">
+    render() {
+        return `
+        <div class="card " style="width: 25rem;">
         <img src="${this.img}" class="card-img-top" alt="${this.type} restaurant">
         <div class="card-body">
             <ul>
-                <li class="card-text font-weight-bold">${this.type} Restaurant</li>
+                <li class="card-text font-weight-bold">${this.name} ${this.type} Restaurant</li>
                 <li class="card-text">Location : ${this.address}, ${this.zipCode}, ${this.city}</li>
-                <li class="card-text">Contact us under: ${this.telephoneNumber} Restaurant</li>
-                <li class="card-text">More info under: ${this.webAddress} Restaurant</li>
+                <li class="card-text">Contact us under: ${this.telephoneNumber}</li>
+                <li class="card-text">More information <a href='${this.webAddress}'>here</a> </li>
             </ul>
         </div>
       </div>
@@ -53,14 +63,64 @@ class Restaurant extends LocationA {
     }
 }
 
+class Events extends LocationA {
+    webAddress;
+    eventDate;
+    eventTime;
+    ticketPrice;
+    eventLocation;
+    constructor(a, b, c, d, name, e, f, g, h, i) {
+        super(a, b, c, d, name);
+        this.webAddress=e;
+        this.eventDate = f;
+        this.eventTime = g;
+        this.ticketPrice = h;
+        this.eventLocation= i;
+    }
+    render() {
+        return `
+        <div class="card" style="width: 25rem;">
+        <img src="${this.img}" class="card-img-top" alt="${this.name} restaurant">
+        <div class="card-body">
+            <ul>
+                <li class="card-text font-weight-bold">${this.name}</li>
+                <li class="card-text">Location :${this.eventLocation} ${this.address}, ${this.zipCode}, ${this.city}</li>
+                <li class="card-text">Event timings: ${this.eventTime}, ${this.eventDate}</li>
+                <li class="card-text">Entry fee per person: ${this.ticketPrice}€ </li>
+                <li class="card-text">More information <a href='${this.webAddress}'>here</a> </li>
+            </ul>
+        </div>
+      </div>
+        `
+    }
+}
+
+//landmark content
+new LocationA(
+    'Vienna',
+    '1010',
+    'St. Charles Church',
+    'img/Karlskirche.jpg',
+    'St. Charles Church'
+);
+
+new LocationA(
+    'Vienna',
+    '1030',
+    'Maxingstraße 13b',
+    'img/Zoo.jpg',
+    'Vienna Zoo'
+);
+
 //restaurant content
 new Restaurant(
     'Vienna',
     '1050',
     'Kettenbrückengasse 19',
     'img/Lemon-leaf.png',
+    'Lemon Leaf',
     '+43(1)5812308',
-    'Lemon Leaf Thai',
+    'Thai',
     'www.lemonleaf.at'
 );
 new Restaurant(
@@ -68,13 +128,41 @@ new Restaurant(
     '1050',
     'Schönbrunner Straße 21',
     'img/Sixta.png',
+    'SIXTA',
     '+43 1 58 528 56 or +43 1 58 528 56',
-    'SIXTA Traditional Austrian',
+    'Traditional Austrian',
     'http://www.sixta-restaurant.at/'
 );
 
-console.table(arr);
+// event content
+new Events (
+    'Vienna',
+    '1150',
+    'Roland Rainer Platz 1',
+    'img/Kris-k.jpg',
+    'Kris Kristofferson',
+    'http://kriskristofferson.com/',
+    'Fr., 15.11.2021',
+    '20:00',
+    '58,50',
+    'Wiener Stadthalle Halle F'
+);
 
+new Events (
+    'Vienna',
+    '1150',
+    'Roland Rainer Platz 1',
+    'img/Lenny-k.jpg',
+    'Lenny Kravitz',
+    'www.lennykravitz.com/',
+    'Sat., 09.112.2029',
+    '19:30',
+    '47,80',
+    'Wiener Stadthalle Halle D'
+);
+
+
+// print out class content on to the html file using the arr
 for (let i of arr) {
     document.getElementsByClassName('result')[0].innerHTML += `${i.render()}`;
-  }
+}
